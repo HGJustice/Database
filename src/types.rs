741b3fc298bfab1dd6938d2d901bee3f9b3c8b74
HGJustice <125309498+HGJustice@users.noreply.github.com>
2025-02-15 17:48:38 +0000

    workign on database

--- src/types.rs
+++ src/types.rs
@@ -1,84 +1,77 @@
 use std::collections::HashMap;
 use crate::errors::{DatabaseErrors, TransactionErrors};
 use std::sync::Arc;  
-use tokio::sync::Mutex; 
+use tokio::sync::RwLock; 
 
 #[derive(Clone)]
-struct Data<T> {
-    item: T,
+struct Data {
+    item: String,
 }
 
-impl<T> Data<T> {
-    fn new(item: T) -> Data<T> {
+impl Data {
+    fn new(item: String) -> Data {
         Data {
             item
         }
     }
 }
 
-enum DatabaseOperations<T> {
-    Insert(u32, Data<T>), 
+enum DatabaseOperations {
+    Insert(u32, Data), 
     Get(u32),
-    Update(u32, Data<T>),
+    Update(u32, Data),
     Delete(u32)
 }
 
-pub struct Database<T> {
-    primary_key: Mutex<u32>,
-    storage: Mutex<HashMap<u32, Data<T>>>
+pub struct Database {
+    primary_key: RwLock<u32>,
+    storage: RwLock<HashMap<u32, Data>>
 }
 
-impl<T: Clone> Database<T> {
-
-    pub fn new() -> Arc<Database<T>> {
-       Arc::new( Database { primary_key: Mutex::new(1), storage: Mutex::new(HashMap::new())})
+impl Database {
+    pub fn new() -> Arc<Database> {
+       Arc::new( Database { primary_key: RwLock::new(1), storage: RwLock::new(HashMap::new())})
     }
 
-    async fn insert_data(&self, data: T) -> Result<(), DatabaseErrors> {
-        let key = *self.primary_key.lock().await;
-
-        self.storage.lock().await.insert(key, Data::new(data));
-        let updated_key = self.primary_key.lock().await.checked_add(1).unwrap();
-        *self.primary_key.lock().await = updated_key;
+    async fn insert_data(&self, data: String) -> Result<(), DatabaseErrors> {
+        let mut key = self.primary_key.write().await;  
+    
+        self.storage.write().await.insert(*key, Data::new(data));
+        *key = key.checked_add(1).unwrap();  
+       
         Ok(())
     }
 
-    async fn get_data(&self, key: u32) -> Result<Data<T>, DatabaseErrors> {
-        if key >= *self.primary_key.lock().await {
+    async fn get_data(&self, key: u32) -> Result<Data, DatabaseErrors> {
+        if key >= *self.primary_key.read().await {
             return Err(DatabaseErrors::InvalidKeyError);
         }
-        let result = self.storage.lock().await.get(&key).cloned().ok_or(DatabaseErrors::InvalidKeyError)?;
+        let result = self.storage.read().await.get(&key).cloned().ok_or(DatabaseErrors::InvalidKeyError)?;
         Ok(result)
     }
 
-    async fn update_data(self, key: u32, update: T ) -> Result<(), DatabaseErrors> {
-        if key >= *self.primary_key.lock().await {
+    async fn update_data(&self, key: u32, update: String ) -> Result<(), DatabaseErrors> {
+        if key >= *self.primary_key.read().await {
             return Err(DatabaseErrors::InvalidKeyError)
         }
-        let mut result = self.storage.lock().await.get_mut(&key).cloned().unwrap(); //explain this error when remove cloned
-        result.item = update;
+        self.storage.write().await.get_mut(&key).ok_or(DatabaseErrors::KeyNotFound)?
+        .item = update;
+        
         Ok(())
     }
 
     async fn delete_data(&self, key: u32) -> Result<(), DatabaseErrors> {
-        if key >= *self.primary_key.lock().await {
+        if key >= *self.primary_key.read().await {
             return Err(DatabaseErrors::InvalidKeyError);
         }
-        self.storage.lock().await.remove(&key);
+        self.storage.write().await.remove(&key);
         Ok(())
+    }
 
+    async fn begin_transaction(&self) -> Transaction {
+        Transaction { operations: Vec::new(), tx_state: TransactionState::New, database_state: Arc::clone(self) }
     }
 }
-
-// impl Database<String> {
-//     async fn get_string_data(&self, key: u32) -> Result<Data<String>, DatabaseErrors> {
-//         if key >= *self.primary_key.lock().await {
-//             return Err(DatabaseErrors::InvalidKeyError);
-//         }
-//         let result = self.storage.lock().await.get(&key).cloned().ok_or(DatabaseErrors::InvalidKeyError)?;
-//         Ok(result)
-//     }
-// }
 
 enum TransactionState {
     New,
@@ -86,30 +79,26 @@ enum TransactionState {
     Commited,
 }
 
-struct Transaction<T> {
-    operations: Vec<DatabaseOperations<T>>,
-    state: TransactionState,
+struct Transaction {
+    operations: Vec<DatabaseOperations>,
+    tx_state: TransactionState,
+    database_state: Arc<Database>
 }
 
-impl<T> Transaction<T> {
-    fn new() -> Transaction<T> {
-        Transaction { operations: Vec::new(), state: TransactionState::New }
-    }
+impl Transaction {
 
-    fn commit_changes() -> Result<(), TransactionErrors> {
+    fn add_operation(&mut self, operation: DatabaseOperations) -> Result<(), TransactionErrors> {
         todo!();
+        //push crud operartions on the vector which will be executed on the database
     }
 
-    fn rollback_changes() -> Result<(), TransactionErrors> {
+    fn commit(&mut self,) -> Result<(), TransactionErrors> {
         todo!();
+        //match statment which executes the operation from the transaction vector on the database_state 
     }
 
-    fn migrate_changes() -> Result<(), TransactionErrors> {
+    fn roll_back(&mut self,) -> Result<(), DatabaseErrors> {
         todo!();
+        //clear / delete the current transaction instnce if on one and is empty 
     }
-}
-
-enum Actions {
-    Read,
-    Wrtie    //????
 }
